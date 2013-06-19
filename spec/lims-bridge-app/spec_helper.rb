5c1c935807060921dfafda5b180ba0f4a6059e26
Loic <llh1@sanger.ac.uk>
2013-06-19 14:53:52 +0100

    Add specs

--- spec/lims-bridge-app/spec_helper.rb
+++ spec/lims-bridge-app/spec_helper.rb
@@ -4,10 +4,11 @@ require 'sequel'
 shared_context "test database" do
   let(:db) { Sequel.sqlite 'test.db' }
   after(:each) do
-    seed_tables = ["maps", "uuids"]
+    seed_tables = ["maps", "uuids", "barcode_prefixes"]
     db.tables.each do |table|
       db[table.to_sym].delete unless seed_tables.include?(table.to_s)
     end
     db[:uuids].where{id > 6}.delete
+    db[:barcode_prefixes].where{id > 1}.delete
   end
 end
