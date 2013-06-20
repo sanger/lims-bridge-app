aca6d53233319e67b110cb86b76d16ada991d350
Matthew Denner <md12@sanger.ac.uk>
2013-06-20 07:58:08 +0100

    Enable ENV to control environment

--- script/start_plate_management.rb
+++ script/start_plate_management.rb
@@ -1,11 +1,14 @@
+ENV["LIMS_BRIDGE_ENV"] = "development" unless ENV["LIMS_BRIDGE_ENV"]
+
 require 'yaml'
 require 'lims-bridge-app'
 require 'logging'
 
 module Lims
   module BridgeApp
-    amqp_settings = YAML.load_file(File.join('config','amqp.yml'))["production"] 
-    mysql_settings = YAML.load_file(File.join('config','database.yml'))["production"] 
+    env = ENV["LIMS_BRIDGE_ENV"]
+    amqp_settings = YAML.load_file(File.join('config','amqp.yml'))[env]
+    mysql_settings = YAML.load_file(File.join('config','database.yml'))[env]
 
     creator = PlateCreator::StockPlateConsumer.new(amqp_settings, mysql_settings)
     creator.set_logger(Logging::LOGGER)
