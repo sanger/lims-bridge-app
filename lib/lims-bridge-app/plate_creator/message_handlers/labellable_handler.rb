324956306f4f31927d4fd6bec44b7f43253fe02d
Loic <llh1@sanger.ac.uk>
2013-06-26 17:08:28 +0100

    Pass the date to set_barcode_a_plate method

--- lib/lims-bridge-app/plate_creator/message_handlers/labellable_handler.rb
+++ lib/lims-bridge-app/plate_creator/message_handlers/labellable_handler.rb
@@ -10,12 +10,12 @@ module Lims::BridgeApp::PlateCreator
         begin 
           if s2_resource.has_key?(:labellables)
             s2_resource[:labellables].each do |labellable|
-              set_barcode_to_a_plate(labellable[:labellable])
+              set_barcode_to_a_plate(labellable[:labellable], labellable[:date])
               plate_uuid = labellable[:labellable].name
               bus.publish(plate_uuid)
             end
           else
-            set_barcode_to_a_plate(s2_resource[:labellable])
+            set_barcode_to_a_plate(s2_resource[:labellable], s2_resource[:date])
             plate_uuid = s2_resource[:labellable].name
             bus.publish(plate_uuid)
           end
