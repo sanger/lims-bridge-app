69c510604d0aa8d1e4c36efe499dae7be8132cba
Loic <llh1@sanger.ac.uk>
2013-07-04 10:09:48 +0100

    Swap message handler

--- lib/lims-bridge-app/plate_creator/message_handlers/swap_samples_handler.rb
+++ lib/lims-bridge-app/plate_creator/message_handlers/swap_samples_handler.rb
@@ -3,11 +3,36 @@ require 'lims-bridge-app/plate_creator/base_handler'
 module Lims::BridgeApp::PlateCreator
   module MessageHandler
     class SwapSamplesHandler < BaseHandler
-        
+
       private
 
       def _call_in_transaction
+        begin
+          s2_resource[:resources].each do |resource|
+            plate_uuid = resource[:uuid]
+            location_samples = resource[:sample_uuids]
+            date = resource[:date]
+            swaps = swaps_for(plate_uuid) 
 
+            swap_samples(plate_uuid, location_samples, swaps, date)
+            bus.publish(plate_uuid)
+          end
+        rescue Sequel::Rollback, PlateNotFoundInSequencescape, UnknownSample => e
+          metadata.reject(:requeue => true)
+          log.info("Error swapping samples in Sequencescape: #{e}")
+          raise Sequel::Rollback
+        else
+          metadata.ack
+          log.info("Swap sample message processed and acknowledged")
+        end
+      end
+
+      # @param [String] plate_uuid
+      # @return [Hash]
+      def swaps_for(plate_uuid)
+        s2_resource[:swaps].each do |swap_parameters|
+          return swap_parameters["swaps"] if swap_parameters["resource_uuid"] == plate_uuid
+        end
       end
     end
   end
