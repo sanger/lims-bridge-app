13cb04e79546ef13375b126c2a06cde2ec931f2b
Loic <llh1@sanger.ac.uk>
2014-01-10 10:11:13 +0000

    Refactor plates to plates decoder

--- lib/lims-bridge-app/decoders/plate_decoder.rb
+++ lib/lims-bridge-app/decoders/plate_decoder.rb
@@ -2,6 +2,8 @@ require 'lims-bridge-app/base_decoder'
 require 'lims-laboratory-app/laboratory/plate'
 require 'lims-bridge-app/decoders/plate_like_shared'
 require 'lims-bridge-app/decoders/single_transfer_shared'
+require 'lims-bridge-app/decoders/gel_decoder'
+require 'lims-bridge-app/decoders/tube_rack_decoder'
 
 module Lims::BridgeApp
   module Decoders
@@ -18,6 +20,7 @@ module Lims::BridgeApp
       end
     end
 
+
     class PlateTransferDecoder < PlateDecoder
       include SingleTransferShared
 
@@ -26,6 +29,30 @@ module Lims::BridgeApp
       # @return [Hash]
       def decode_plate_transfer
         _decode_transfer("plate", PlateDecoder)
+      end
+    end
+
+
+    class TransferPlatesToPlatesDecoder < PlateDecoder
+
+      private
+
+      # @return [Hash]
+      def decode_transfer_plates_to_plates
+        plates = []
+        ["sources", "targets"].each do |key|
+          resource_hash["result"][key].each do |asset|
+            asset_decoder = case asset.keys.first
+                            when "plate" then PlateDecoder.new(asset, options)
+                            when "tube_rack" then TubeRackDecoder.new(asset, options)
+                            when "gel" then GelDecoder.new(asset, options)
+                            end
+
+            plates << asset_decoder.call
+          end
+        end
+
+        {:plates => plates, :transfer_map => resource_hash["transfers"]}
       end
     end
   end
--- spec/lims-bridge-app/decoders/transfer_plates_to_plates_decoder_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'lims-bridge-app/decoders/spec_helper'
-require 'lims-bridge-app/decoders/messages_factory_helper'
-require 'lims-bridge-app/decoders/transfer_plates_to_plates_decoder'
-
-module Lims::BridgeApp::Decoders
-  describe TransferPlatesToPlatesDecoder do
-    pending
-  end
-end
